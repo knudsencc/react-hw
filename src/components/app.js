05211395714528e1e3c42730dbdc7642390aa7d5
Cameron KNUDSEN <tmphotock@gmail.com>
2018-03-26 17:46:56 -0600

    background styling and heading

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>DevCamp React Starter</h1>
-      </div>
+        <div className= 'background'>
+          <div className= 'backgroundSkew'>
+            <div className= 'test-heading'>
+              <h1>Test Run</h1>
+              <div className= 'test-subheading'>
+              This is just a test to see how much I know.
+              </div>
+            
+                </div>
+                <div className= 'backgroundSkew-2'>
+                </div>
+            </div>
+        </div>
     );
   }
 }
